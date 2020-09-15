c24df948942798b8a7e8c3d2ffc09d78c47f76cc
teveritt <teveritt@appliedrelevance.com>
2020-09-15 16:28:59 -0400

    added Tip test suite

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -417,6 +417,76 @@ suite("Note Alerts", () => {
   });
 });
 
+suite("Tip Alerts", () => {
+  test("Ranged selection", () => {
+    return testCommand(
+      "toggleTip",
+      "[This is just a plain tip}",
+      "[>[!TIP]\n>\n>This is just a plain tip\n}"
+    );
+  });
+
+  test("Multiline ranged selection", () => {
+    return testCommand(
+      "toggleNote",
+      "[This is just a" + newLine + "plain note}",
+      "[>[!NOTE]\n>\n>This is just a\nplain note\n}"
+    );
+  });
+
+  test("Multiline ranged selection with extra newline", () => {
+    return testCommand(
+      "toggleNote",
+      "[This is just a" + newLine + "plain note}" + newLine,
+      "[>[!NOTE]\n>\n>This is just a\nplain note\n}"
+    );
+  });
+
+  test("Multiline ranged selection while selecting extra newline", () => {
+    return testCommand(
+      "toggleNote",
+      "[This is just a" + newLine + "plain note" + newLine + "}",
+      "[>[!NOTE]\n>\n>This is just a\nplain note\n\n}"
+    );
+  });
+
+  test("Collapsed selection", () => {
+    return testCommand(
+      "toggleNote",
+      "Just a plain note^",
+      "[>[!NOTE]\n>\n>Just a plain note\n}"
+    );
+  });
+
+  test("Toggles with ranged selection", () => {
+    return testCommand(
+      "toggleNote",
+      "[>[!NOTE]" +
+        newLine +
+        ">" +
+        newLine +
+        ">This is just a plain note" +
+        newLine +
+        "}",
+      "[This is just a plain note}"
+    );
+  });
+
+  test("Toggles with multi-line ranged selection", () => {
+    return testCommand(
+      "toggleNote",
+      "[>[!NOTE]" +
+        newLine +
+        ">" +
+        newLine +
+        ">This is just a" +
+        newLine +
+        "plain note}",
+      "[This is just a\nplain note}"
+    );
+  });
+});
+
 suite("Citations", () => {
   test("Collapsed selection", () => {
     return testCommand(
