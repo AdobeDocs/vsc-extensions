981058d5a31cb9b9014c0ed53f01cdd6a34495a0
George Everitt <geveritt@appliedrelevance.com>
2021-10-07 09:43:17 -0400

    Added target link prompt. Basic functionality works - but no edge cases at all work yet. Snapshot to merge back into master branch.

--- src/lib/commands/toggle-link.ts
+++ src/lib/commands/toggle-link.ts
@@ -11,14 +11,15 @@ import {
 } from "../editorHelpers";
 
 interface LinkProps {
-  text: string | undefined;
-  url: string | undefined;
+  text?: string;
+  url?: string;
+  target?: string;
 }
 function addLinkTag(linkProps: LinkProps): void | Thenable<void> {
   surroundSelection("[" + linkProps.text, "](" + linkProps.url + ")");
 }
 const wordMatch: string = "[A-Za-z\\u00C0-\\u017F]";
-const markdownLinkRegex: RegExp = /^\[.+\]\(.+\)$/;
+const markdownLinkRegex: RegExp = /^\[.+\]\(.+\)(\{.+\})|(.*)$/;
 const markdownLinkWordPattern: RegExp = new RegExp(
   "[.+](.+)|" + wordMatch + "+"
 );
@@ -30,20 +31,25 @@ export function toggleLink(): void {
   let selection: Selection = editor.selection;
 
   if (!isAnythingSelected()) {
+    // Nothing is selected. See if the surrounding text includes something that resembles
+    // a Markdown link markup.
     const surroundingWord = getSurroundingWord(
       editor,
       selection,
       markdownLinkWordPattern
     );
 
+    // If we found a hyperlink, select it.
     if (surroundingWord) {
       selection = editor.selection = surroundingWord;
     }
   }
 
+  // Is something selected from the previous block?
   if (isAnythingSelected()) {
+    // Something is selected.
     if (isMatch(markdownLinkRegex)) {
-      //Selection is a MD link, replace it with the link text
+      //Selection is a Markdown link expression, replace it with the link text
       replaceSelection((text) => {
         const mdLink: RegExpMatchArray | null = text.match(/\[(.+)\]/);
         return mdLink ? mdLink[1] : text;
@@ -57,15 +63,19 @@ export function toggleLink(): void {
     }
   }
 
-  let linkObj: LinkProps = { text: "", url: "" };
-
-  promptForInput("Enter link display text")
-    .then((text) => {
-      linkObj.text = text;
-      return promptForInput("Enter link target URL");
-    })
+  let linkObj: LinkProps = { text: "", url: "", target: "" };
+  promptForInput("Enter Link URL")
     .then((url) => {
       linkObj.url = url;
+      linkObj.text = url;
+      return promptForInput("Enter link text");
+    })
+    .then((text) => {
+      linkObj.text = text;
+      return promptForInput("Enter link target");
+    })
+    .then((target) => {
+      linkObj.target = target;
       return addLinkTag(linkObj);
     });
 }
