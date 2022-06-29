85efa3b3f68188d6154566c037ebb620ef123d95
George Everitt <geveritt@appliedrelevance.com>
2022-06-29 15:01:01 -0400

    Added | to borders of table sample template

--- src/lib/tables.ts
+++ src/lib/tables.ts
@@ -5,11 +5,11 @@ import { isAnythingSelected } from './editorHelpers';
 
 var sampleTable = [
     "",
-    "Column A | Column B | Column C",
-    "---------|----------|---------",
-    " A1 | B1 | C1",
-    " A2 | B2 | C2",
-    " A3 | B3 | C3"
+    "|Column A | Column B | Column C |",
+    "|---------|----------|---------|",
+    "| A1 | B1 | C1 |",
+    "| A2 | B2 | C2 |",
+    "| A3 | B3 | C3 |"
     ].join("\n");
 
 export function addTable(addHeader:boolean=false) {
